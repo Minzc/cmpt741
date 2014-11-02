2a02d06bb26b48767b9a33f59ee327a09c612a92
Minzc <congzicun@gmail.com>
2014-11-02 14:49:49 -0800

    hello world

---
+++ src/main/java/ca/sfu/cmp741/HelloWorld.java
@@ -0,0 +1,11 @@
+package ca.sfu.cmp741;
+
+/**
+ * @author congzicun
+ * @since 2014-11-02 2:49 PM
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
